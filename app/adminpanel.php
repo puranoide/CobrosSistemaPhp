d6cca00dc59509555e4cb63d333805547ace3163
puranoide <acostasanchezangelgabriel@gmail.com>
2024-12-25 22:40:30 -0500

    Update adminpanel.php

--- app/adminpanel.php
+++ app/adminpanel.php
@@ -2,12 +2,53 @@
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    function sendMessage($con, $id, $message){
+        date_default_timezone_set('America/Lima');
+        $date= new DateTime();
+        $formattedDate = $date->format('Y-m-d'); // Asignar el resultado a una variable
+        $stmt = $con->prepare("INSERT INTO mesagessends (message, clientsId, dateMessageSend) VALUES (?, ?, ?)");
+        $stmt->bind_param("sis", $message, $id, $formattedDate);
+        return $stmt->execute() ? $con->insert_id : $con->error;
+    }
+    function sendMail(){
+        $para="puranogame@gmail.com";
+        $asunto="Nuevo Registro correo'";
+        $mensaje="mensaje de prueba";
+        $cabecera="From: no-reply@darkslateblue-fly-946582.hostingersite.com";
+        if(mail($para,$asunto,$mensaje,$cabecera)){
+            return true;
+        }else{
+            return false;
+        }
+    }
     print_r($_POST);    
+
+    $action=$_POST['action'];
+    
+    
+    switch ($action) {
+        case 'sendMenssage':
+            require_once('../config/bd.php');
+            $id=$_POST['id'];
+            $message=$_POST['message'];
+            $respuesta = sendMessage($con, $id, $message);
+            if ($respuesta === false) {
+                header("Location:../app/adminpanel.php?user=admin&pass=3_0i1n{-TJ-XUBQ_2azQ-;;3h1PDygV-]4*7}kCC))yR-{e3c(en[5=-/}bXS:g*:4-TcG[GZ{XZh-5@jXByU-dt-YA0DfWqXY)-uQYZ6*c5_4&message=Error al enviar el mensaje");
+            }
+            sendMail();
+            header("Location:../app/adminpanel.php?user=admin&pass=3_0i1n{-TJ-XUBQ_2azQ-;;3h1PDygV-]4*7}kCC))yR-{e3c(en[5=-/}bXS:g*:4-TcG[GZ{XZh-5@jXByU-dt-YA0DfWqXY)-uQYZ6*c5_4&message=mensaje enviado id=$respuesta");
+            print_r($respuesta);
+            break;
+        
+        default:
+            # code...
+            break;
+    }
 }else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     
 $user=$_GET['user'];
 $pass=$_GET['pass'];
-
+$message=isset($_GET['message']) ? $_GET['message'] : null;
 if ($user == 'admin' && $pass == '3_0i1n{-TJ-XUBQ_2azQ-;;3h1PDygV-]4*7}kCC))yR-{e3c(en[5=-/}bXS:g*:4-TcG[GZ{XZh-5@jXByU-dt-YA0DfWqXY)-uQYZ6*c5_4'):
     function listClients($con)
     {
@@ -29,13 +70,7 @@ if ($user == 'admin' && $pass == '3_0i1n{-TJ-XUBQ_2azQ-;;3h1PDygV-]4*7}kCC))yR-{
     require_once('../config/bd.php');
     $clients = listClients($con);
 
-    function SendMessage($con, $id, $message){
-        $date= new DateTime();
-        $stmt = $con->prepare("INSERT INTO mesagessends SET message=?,clienteId=?,date=?");
-        $stmt->bind_param("ssi", $message, $id, $date->format('Y-m-d'));
-        return $stmt->execute() ? $con->insert_id : $con->error;
-        
-    }
+ 
 
     function listClientstodayPayment($con){
         date_default_timezone_set('America/Lima');
@@ -68,6 +103,10 @@ if ($user == 'admin' && $pass == '3_0i1n{-TJ-XUBQ_2azQ-;;3h1PDygV-]4*7}kCC))yR-{
         <title>admin view</title>
     </head>
     <body>
+         <?php if (isset($message)): ?>
+            <p><?= $message ?></p>
+        <?php endif; ?>   
+
         <h1>admin panel</h1>
         <table>
             <thead>
@@ -128,7 +167,8 @@ if ($user == 'admin' && $pass == '3_0i1n{-TJ-XUBQ_2azQ-;;3h1PDygV-]4*7}kCC))yR-{
                         </td>
                         <td>
                             <form action="adminpanel.php" method="POST" style="display:inline;">
-                                <input type="hidden" name="action" value="edit">
+                                <input type="hidden" name="action" value="sendMenssage">
+                                <input type="hidden" name="message" value="<?= $client['message'] ?>">
                                 <input type="hidden" name="id" value="<?= $client['id'] ?>">
                                 <button type="submit">enviar</button>
                             </form>
